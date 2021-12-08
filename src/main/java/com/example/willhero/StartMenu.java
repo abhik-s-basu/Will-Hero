d9b0eaa982a1f9b22fd29573286d27e718ecb446
abhik-s-basu <abhik20165@iiitd.ac.in>
2021-12-08 21:25:26 +0530

    made basic layouts (start menu & main menu)

---
+++ src/main/java/com/example/willhero/StartMenu.java
@@ -0,0 +1,52 @@
+package com.example.willhero;
+
+import javafx.animation.PauseTransition;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class StartMenu extends Application implements Initializable {
+    public static Stage myStage;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        try{
+            Image icon = new Image(new File("file:src/main/resources/Assets/Knight.png").toURI().toString());
+            stage.getIcons().add(icon);
+//            stage.getIcons().add(new Image("file:src/main/resources/Assets/Knight.png"));
+            Parent root = FXMLLoader.load(getClass().getResource("StartMenu.fxml"));
+            Scene scene = new Scene (root);
+            stage.setScene(scene);
+            stage.setResizable(false);
+            stage.show();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
---
+++ src/main/java/com/example/willhero/StartMenuController.java
@@ -0,0 +1,47 @@
+package com.example.willhero;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class StartMenuController  {
+
+    @FXML
+    ImageView startImage;
+
+    @FXML
+    Text clickToStart;
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void gotoStartMenu(MouseEvent event) throws IOException{
+        root= FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+
+
+
+
+
+
+}
